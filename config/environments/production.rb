d47a028c0af83862df00034cbbafeb6de1ac6c18
Su Nguyen T <vnntsu@gmail.com>
2024-01-19 12:57:20 +0700

    Define app_host in dockerfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts = ['survey-api-staging.nimblehq.co', 'survey-api.nimblehq.co']
+  config.hosts = [ENV.fetch('APP_HOST', '')]
 
   # Settings specified here will take precedence over those in config/application.rb.
 
