cfd76892eae860f497f44bd53c954b680d8616b9
Su Nguyen T <vnntsu@gmail.com>
2024-01-19 11:04:03 +0700

    Add config.host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts = ['survey-api-staging.nimblehq.co', 'https://survey-api.nimblehq.co']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
